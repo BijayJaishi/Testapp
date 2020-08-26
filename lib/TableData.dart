2b4403b2df6720c0ee826e31970224ea2a2e94de
Bijay Jaishi <bjlc2052@gmail.com>
2020-08-26 15:10:56 +0545

    second commit

--- lib/TableData.dart
+++ lib/TableData.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:archive/archive.dart';
 import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
 
 class MyApp extends StatefulWidget {
   @override
@@ -18,6 +23,7 @@ class _MyAppState extends State<MyApp> {
     // TODO: implement initState
     super.initState();
     getExcel();
+    _fetchListItem();
   }
 
   @override
@@ -52,17 +58,17 @@ class _MyAppState extends State<MyApp> {
       children: <Widget>[
         SingleChildScrollView(
             child: SafeArea(
-          child: getTable(),
-        )),
+              child: getTable(),
+            )),
         Positioned(
           child: isLoading
               ? Container(
-                  child: Center(
-                    child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue)),
-                  ),
-                  color: Colors.white.withOpacity(0.8),
-                )
+            child: Center(
+              child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue)),
+            ),
+            color: Colors.white.withOpacity(0.8),
+          )
               : Container(),
         )
       ],
@@ -74,8 +80,8 @@ class _MyAppState extends State<MyApp> {
     final mediaQueryData = MediaQuery.of(context);
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Table(
-        columnWidths:{
+      child:  Table(
+        columnWidths: {
           0: FlexColumnWidth(1.08),
           1: FlexColumnWidth(1.8),
           2: FlexColumnWidth(2.0),
@@ -86,21 +92,22 @@ class _MyAppState extends State<MyApp> {
           7: FlexColumnWidth(1.3),
         },
         border: TableBorder.all(width: 1.0),
+
         children: tableData.map((item) {
           return TableRow(
               children: item.map((row) {
-            return Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  row.toString(),
-                  style: mediaQueryData.orientation == Orientation.portrait
-                      ? TextStyle(fontSize: 8.0, color: Colors.black)
-                      : TextStyle(fontSize: 13.0, color: Colors.black),
-                ),
-              ),
-            );
-          }).toList());
+                return Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      row.toString(),
+                      style: mediaQueryData.orientation == Orientation.portrait
+                          ? TextStyle(fontSize: 8.0, color: Colors.black)
+                          : TextStyle(fontSize: 13.0, color: Colors.black)
+                    ),
+                  ),
+                );
+              }).toList());
         }).toList(),
       ),
     );
@@ -129,13 +136,39 @@ class _MyAppState extends State<MyApp> {
       isLoading = true;
     });
     ByteData data = await rootBundle.load("assets/excelConvertedData.xlsx");
+
     var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    print('ByteData:$bytes');
     var excel = Excel.decodeBytes(bytes, update: true);
+    print("pexcel:$excel");
     tableData.clear();
     for (var table in excel.tables.keys) {
       for (var row in excel.tables[table].rows) {
         tableData.add(row);
       }
     }
+  }
+
+  _fetchListItem() async {
+
+    var _downloadData = List<int>();
+    HttpClient client = new HttpClient();
+    client.getUrl(Uri.parse("https://file-examples-com.github.io/uploads/2017/02/file_example_XLS_100.xls"))
+        .then((HttpClientRequest request) {
+      return request.close();
+    })
+        .then((HttpClientResponse response) {
+      response.listen((d) => _downloadData.addAll(d),
+          onDone: () {
+
+            Archive archive = new ZipDecoder().decodeBytes(_downloadData);
+
+        print('Data:$archive');
+//        var excel = Excel.decodeBytes(_downloadData, update: true);
+//        print('eccell:$excel');
+          }
+      );
+//      response.transform(utf8.decoder).listen((contents) => print("Content:$contents"));
+    });
   }
 }
